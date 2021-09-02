dff8d16a870cda8f828180d8d1e616db5bfc867b
Jesus Serrano <jesus.serrano@muuklabs.com>
2021-09-01 17:44:34 -0700

    Get and Set methods.

--- src/com/qaminds/Television.java
+++ src/com/qaminds/Television.java
@@ -2,6 +2,12 @@ package com.qaminds;
 
 public class Television {
 
+    private String marca;
+    private int tamano;
+    private double precio;
+    private String tipo;
+    Television tv = new Television();
+
     public Television(){
         System.out.println("No hay tv");
     }
@@ -14,4 +20,46 @@ public class Television {
         this(marca,tamano, precio );
         System.out.println("De tipo " + tipo);
     }
+
+    public void setTv(Television tv) {
+        this.tv = tv;
+    }
+
+    public Television getTv(){
+        return this.tv;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getMarca(){
+        return this.marca;
+    }
+
+    public void setTamano(int tamano){
+        this.tamano = tamano;
+    }
+
+    public int getTamano(){
+        return this.tamano;
+    }
+
+    public void setPrecio(double precio){
+        this.precio = precio;
+    }
+
+    public double getPrecio(){
+        return this.precio;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+
+    public String getTipo(){
+        return this.tipo;
+    }
+
+
 }
